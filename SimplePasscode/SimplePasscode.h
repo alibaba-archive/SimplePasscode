7f95e923a0869feca3b5cc3f75caeabde3a257cc
Zhu Shengqi <zetasq@gmail.com>
2016-05-07 10:31:27 +0800

    Initial Commit

---
+++ SimplePasscode/SimplePasscode.h
@@ -0,0 +1,19 @@
+//
+//  SimplePasscode.h
+//  SimplePasscode
+//
+//  Created by Zhu Shengqi on 5/7/16.
+//  Copyright © 2016 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimplePasscode.
+FOUNDATION_EXPORT double SimplePasscodeVersionNumber;
+
+//! Project version string for SimplePasscode.
+FOUNDATION_EXPORT const unsigned char SimplePasscodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplePasscode/PublicHeader.h>
+
+
